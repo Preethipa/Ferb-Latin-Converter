e95ad59c74484fdb32c90e6c8e19bdba26c04aae
PREETHI <preethirajavel06@gmail.com>
2022-11-29 13:58:08 +0530

    show output

--- trans-app.js
+++ trans-app.js
@@ -5,7 +5,7 @@ var outputDiv = document.querySelector("#output");
 function clickHandler(){
     //console.log("clicked");
     //console.log(inputTxt.value);
-    outputDiv.innerText = "jjjjjaaaa " + inputTxt.value;
+    outputDiv.innerText = inputTxt.value;
     //console.log(outputDiv.innerText);
 }
 
