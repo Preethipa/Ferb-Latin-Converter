7485ea8d54f78fa1efe7f067c5c293a57eee20f1
PREETHI <preethirajavel06@gmail.com>
2022-11-29 19:07:36 +0530

    processing done

--- trans-app.js
+++ trans-app.js
@@ -2,11 +2,32 @@ var inputTxt = document.querySelector("#ip-txt");
 var transBtn = document.querySelector("#trans-btn");
 var outputDiv = document.querySelector("#output");
 
-function clickHandler(){
-    //console.log("clicked");
-    //console.log(inputTxt.value);
-    outputDiv.innerText = inputTxt.value;
-    //console.log(outputDiv.innerText);
+var serverURL = "https://api.funtranslations.com/translate/ferb-latin.json";
+
+function getTranslationURL(input){
+    return serverURL + "?" + "text=" + input;
 }
+
+function errorHandler(error){
+    console.log("error occured", error);
+    alert("Something wrong with server try again later!");
+}
+
+function clickHandler(){
+   
+    var inputText = inputTxt.value;
+    //outputDiv.innerText = inputTxt.value;
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => {
+        var TranslatedText = json.contents.translated;
+        outputDiv.innerText = TranslatedText;
+        //console.log(TranslatedText);
+    })
+    
+        
+    .catch(errorHandler)
+    
+};
 
 transBtn.addEventListener("click", clickHandler);
